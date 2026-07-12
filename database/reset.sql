f5acd44f8f2d79588fc50983d3021215d920e87b
vibhamadabushi <madabushivibha@gmail.com>
2026-07-12 12:54:48 +0530

    add reset.sql

---
+++ database/reset.sql
@@ -0,0 +1,55 @@
+-- =====================================================
+-- EcoSphere Reset Script
+-- =====================================================
+
+TRUNCATE TABLE
+
+employee_badges,
+
+reward_redemptions,
+
+points_ledger,
+
+notifications,
+
+challenge_participations,
+
+employee_participations,
+
+policy_acknowledgements,
+
+compliance_issues,
+
+audits,
+
+carbon_transactions,
+
+challenges,
+
+csr_activities,
+
+environmental_goals,
+
+product_esg_profiles,
+
+badges,
+
+rewards,
+
+esg_policies,
+
+emission_factors,
+
+categories,
+
+department_scores,
+
+users,
+
+departments,
+
+org_esg_settings,
+
+organizations
+
+CASCADE;
